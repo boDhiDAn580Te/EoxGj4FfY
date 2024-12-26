6e6831a406b9e6d035ae15172e173df3520aed48
boDhiDAn580Te <crusts.ounce.0g@icloud.com>
2024-12-26 23:31:36 +0100

    优化了 网络请求 模块的性能

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'no-extra-parens': 0,
+    '@typescript-eslint/no-extra-parens': 1,
+    quotes: [1, 'single']
+  },
+  extends: ['plugin:@typescript-eslint/recommended']
+}
